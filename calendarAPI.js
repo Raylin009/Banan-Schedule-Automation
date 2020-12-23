2719530a34126945c4e9b20866f20170d610400d
Ray Lin <raylin009@gmail.com>
2020-12-22 18:25:50 -0800

    found BR calendar obj

--- calendarAPI.js
+++ calendarAPI.js
@@ -3,7 +3,7 @@ const readline = require('readline');
 const {google} = require('googleapis');
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
@@ -13,7 +13,7 @@ const TOKEN_PATH = 'calendarToken.json';
 fs.readFile('calendarCredentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Calendar API.
-  authorize(JSON.parse(content), listEvents);
+  authorize(JSON.parse(content), getCalendarListId);
 });
 
 /**
@@ -91,4 +91,35 @@ function listEvents(auth) {
       console.log('No upcoming events found.');
     }
   });
+}
+
+function createEvent (){
+
+}
+
+function getCalendarListId (auth) {
+  const calendar = google.calendar({version: 'v3', auth});
+  calendar.calendarList.list({
+    auth:auth,
+    maxResults: 15,
+    minAccessRole: "owner",
+    showDeleted: false,
+    showHidden: false,
+  }, (err, res) => {
+    if (err){
+      console.log(err)
+    }else{
+      const brCalendarOBJ = findBRCalendar(res.data.items)
+      console.log(brCalendarOBJ)
+    }
+  })
+}
+
+function findBRCalendar (arr) {
+  const [brCalendar] = arr.filter((ele) => ele.description === 'Banana Republic Schedule')
+  console.log(brCalendar)
+}
+
+function addEvent (auth, calendarListId, date, time) {
+
 }
