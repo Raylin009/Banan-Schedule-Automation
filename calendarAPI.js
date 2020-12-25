7718fb7afd764704c1c7e70a67285c69d92a3028
Ray Lin <raylin009@gmail.com>
2020-12-24 18:08:03 -0800

    added delete but have not test yet

--- calendarAPI.js
+++ calendarAPI.js
@@ -1,29 +1,20 @@
 const fs = require('fs');
 const readline = require('readline');
-const {google} = require('googleapis');
+const {google, calendar_v3} = require('googleapis');
 
 const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 const TOKEN_PATH = 'calendarToken.json';
 
-// Load client secrets from a local file.
 fs.readFile('calendarCredentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Calendar API.
   authorize(JSON.parse(content), getCalendarListId);
 });
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
 function authorize(credentials, callback) {
   const {client_secret, client_id, redirect_uris} = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
 
-  // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
     if (err) return getAccessToken(oAuth2Client, callback);
     oAuth2Client.setCredentials(JSON.parse(token));
@@ -31,12 +22,6 @@ function authorize(credentials, callback) {
   });
 }
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
 function getAccessToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
@@ -152,4 +137,17 @@ function addEvent (auth, calendarListId, event) {
     console.log('Event created: %s', event.data.htmlLink);
     console.log(event.data.id)
   });
+}
+
+
+function deleteEvent (auth, calendarId, eventId) {
+  const calendar = google.calendar({vesion: 'v3', auth});
+  calendar.events.delete({
+    calendarId,
+    eventId,
+    sendNotifications: true
+  }, function (err, res) {
+    if (err) {console.log(err)}
+    return res
+  })
 }
