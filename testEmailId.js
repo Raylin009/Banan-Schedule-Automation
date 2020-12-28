c8e3f0a364b4caa8162248122f7b1db838765f92
Ray Lin <raylin009@gmail.com>
2020-12-28 15:12:08 -0800

    added type to seperate the list

--- testEmailId.js
+++ testEmailId.js
@@ -1,12 +1,12 @@
 module.exports.email_Id_List = [
-  { id: '176794e0d843c7c5', threadId: '176794e0d843c7c5' },
-  { id: '17674efad6d4990e', threadId: '17674efad6d4990e' },
+  { id: '176794e0d843c7c5', threadId: '176794e0d843c7c5' , type: 'weekly'},
+  { id: '17674efad6d4990e', threadId: '17674efad6d4990e' , type: 'weekly'},
   { id: '176503c927789c52', threadId: '176503c927789c52' },
-  { id: '17650163a9310979', threadId: '17650163a9310979' },
-  { id: '176312c9d75c37ed', threadId: '176312c9d75c37ed' },
-  { id: '175e91f66509ad4e', threadId: '175e91f66509ad4e' },
-  { id: '175e34b8b3f32201', threadId: '175e34b8b3f32201' },
+  { id: '17650163a9310979', threadId: '17650163a9310979' , type: 'weekly'},
+  { id: '176312c9d75c37ed', threadId: '176312c9d75c37ed' , type: 'weekly'},
+  { id: '175e91f66509ad4e', threadId: '175e91f66509ad4e' , type: 'weekly'},
+  { id: '175e34b8b3f32201', threadId: '175e34b8b3f32201' , type: 'weekly'},
   { id: '175de3b52ca85090', threadId: '175de3b52ca85090' },
   { id: '175d49ec2bb6430b', threadId: '175d49ec2bb6430b' },
-  { id: '175c4fc236fa7270', threadId: '175c4fc236fa7270' }
+  { id: '175c4fc236fa7270', threadId: '175c4fc236fa7270' , type: 'weekly'}
 ]
