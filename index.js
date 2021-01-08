15b0158185c53484fed5bd5fc0d8fa5f2f63ce70
Ray Lin <raylin009@gmail.com>
2021-01-07 19:41:47 -0800

    rebuilding all function here:

--- index.js
+++ index.js
@@ -1,51 +1,105 @@
-const { getBREventList } = require('./calendarAPIv2.js');
+const { getBREventList, initCalendar } = require('./calendarAPIv2.js');
+const { getMessageList, getMessageContent, parseMessageContent, initGmail } = require('./gmailAPIv2.js');
 const fs = require('fs');
 
-// getBREventList()
-// .then(console.log)
-// .catch(console.log)
-
-/*
-
-get schedule from email
-
-  * GET from: no-reply-ams@infor.com && a month ago
-    catagory1:
-      Subject has "Lin, Ray-Jhe, Work Schedule/Horaire de travail/Programa de trabajo,"
-    catagory2:
-      subject has "Schedule Change Alert/Alerte de changement d'horaire/Programar alerta de cambio"
-    catagory3:
-      alert me so I can take a look at it
-
-  * CONSOLIDATE all the scheduled dates from the past month
-    check if the date existes already
-      if it does then check if the time are the same
-        if time is the same then move on
-        if time is different then replace the date with the latest one
-      if the date doesnt exist
-        add the date and time to the master schedule
-    get rid of past events in the array
-add to calender
-
-  * get all the future events 
-
-  * match with the master scheudle 
-    if date exist biut time different, 
-      patch
-    if date deosnt exist
-      add
-  return a log of changes made
-*/
-const pTest = async(cb) => {
-  fs.readFile('gmailCredentials.json', (err, content) => {
-    if (err) return 'Error loading client secret file:'+ err
-    return content
+const get_Email_Ids = async(parameters) => {
+  const gmail = await initGmail()
+  return new Promise((resolve, reject) => {
+    gmail.users.messages.list(parameters, (err, res) => {
+      if (err) {return reject(`Error in get_Email_Ids funcrtion.\nParameters:\n${parameters}\n${err}`)};
+      const { messages } = res.data;
+      resolve(messages)
+    })
   })
 }
-const what = pTest()
 
-console.log(what)
+const get_Email_Content = async(parameters) => {
+  const gmail = await initGmail()
+  return new Promise((resolve, reject) => {
+    gmail.users.messages.get(parameters, (err, res) => {
+      if (err) {return reject(`Error in get_Email_Content function\nParameters:\n${parameters}\n${err}`)};
+      const { data } = res;
+      resolve(data)
+    })
+  })
+}
 
+const parseEmail = (emailObj) => {
+  if(!emailObj) {
+    throw Error(`"parseEmail" function expect input to be valid emailObj but recieved ${emailObj}`)
+  }
+  const { id, payload } = emailObj
+  const metaData = {
+    id,
+    contentType: payload.mimeType,
+  }
+  const reducer = (sto, cur) => {
+    if(!sto){
+      sto = {}
+    }
+    const reqElements = ['From', 'Subject', 'Date', 'Content-Type'];
+    reqElements.forEach((ele) => {
+      if(cur.name === ele){
+        sto
+      }
+    })
+  }
+  payload.headers.reduce()
+  
+}
 
+const auto_update_shift = async() => {
+  const calendar = initCalendar();
+  const gmail = initGmail();
+  /* Phase I : populate schedule*/
+  //in the order of oldest email to the newest
+    //include all the dates in the emal
+  //populate and update the master schedule
 
+  //get emal list
+  const emailListParam = {
+    userId: 'me',
+    includeSpamTrash: false,
+    labelIds: 'INBOX',
+    maxResults: 5,
+    q: 'from:no-reply-ams@infor.com'
+  };
+  const email_Id_List = await get_Email_Ids(emaiListParam)
+  
+  return(email_Id_List)
+  /* 
+  parse email
+    need to get: 
+      id, 
+      payload.headers[
+        From, 
+        Subject,
+        Date,(recieved date)
+        Content-Type: 
+          "text/html; charset=ISO-8859-1" (monthly)
+          "text/plain; charset=UTF-8" (weekly)
+      ]
+      payload.mimeType: 
+        "text/plain" || "text/html"
+      body.data
+  */
 
+  /* Phase II: 
+  //interate through the master schedule
+  //make changes to calendar accordingly  
+  */
+}
+
+/*
+auto_update_shift()
+.then(console.log)
+.catch(console.log)
+*/
+
+// get_Email_Content({
+//   userId: 'me',
+//   id: '176794e0d843c7c5',
+//   format: "full",
+// })
+// .then(console.log)
+// .catch(console.log)
